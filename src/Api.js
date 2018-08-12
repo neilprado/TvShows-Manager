c9cec28dc3212f0362bbb875cbab48d1f70c08ea
neilprado <neiljohn17@gmail.com>
2018-08-12 15:15:11 -0300

    Insertion and listing tv shows

--- src/Api.js
+++ src/Api.js
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
+	baseURL: 'http://localhost:3000/'
 })
 
 export const loadGenres = () => api.get('genres')
+export const saveSeries = (newSeries) => api.post('series', newSeries)
+export const loadShows = (genre) => api.get('series?genre='+genre)
 
 const apis = {
-    loadGenres: loadGenres
+	loadGenres: loadGenres,
+	saveSeries: saveSeries,
+	loadShows: loadShows
 }
 
 export default apis
--- src/App.js
+++ src/App.js
@@ -1,80 +1,52 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import api from './Api.js' 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import NewTvShow from './NewTvShow.js'
+import Shows from './Shows'
+import Home from './Home.js'
+
+// Verificar functional stateless component
+const About = () => <section className="intro-section"><h1> Sobre </h1> </section>
 
 class App extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      genres: [],
-      isLoading: false
-    }
-  }
-  componentDidMount(){
-    this.setState({isLoading:true})
-    api.loadGenres()
-      .then((res) => {
-        this.setState({
-          isLoading: false,
-          genres: res.data
-        })
-      })
-  }
-
-  renderGenreLink(genre){
-    return(
-     <span>&nbsp;<a href=''>{genre}</a>&nbsp;</span>
-    )
-  }
-
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-          <div className="container">
-            <div className="navbar-header page-scroll">
-              <a className="navbar-brand page-scroll" href="#page-top">
+      <Router>
+        <div>
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
                   <img src="images/logo.png" height="30" />
-              </a>
-            </div>
-
-            <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav">
-                <li>
-                  <a href="">Menu item {this.state.count}</a>
-                </li>
-              </ul>
-            </div>
-
-          </div>
-        </nav>
-
-
-        <section id="intro" className="intro-section">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h1><img src="images/logo.png" /></h1>
-                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+                </a>
               </div>
+
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <Link to='/'>Home</Link>
+                  </li>
+                  <li>
+                    <Link to='/new'>New Tv Show</Link>
+                  </li>
+                  <li>
+                    <Link to='/about'>About</Link>
+                  </li>
+                </ul>
+              </div>
+
             </div>
-          </div>
-        </section>
-        <section> 
-          {
-            this.state.isLoading &&
-            <span> Please, wait. Loading now... </span> 
-          }
-          {
-            !this.setState.isLoading &&
-            <div>
-              See TV Shows from: 
-              {this.state.genres.map(this.renderGenreLink)}
-            </div>
-          }
-        </section>
-      </div>
+          </nav>
+          <Route exact path='/' component={Home} />
+          <Route path='/series/:genre' component={Shows} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/new' component={NewTvShow} />
+
+        </div>
+      </Router>
     )
   }
 }
